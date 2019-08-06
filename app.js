5a1afbabe9e45cda26934989aba4920214b02406
Jay Howard <jay@crickethealth.com>
2019-08-06 08:50:18 -0700

    Adds scaffolding for getting/listing all songs and fixes routing

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 import m from "mithril";
 
 const Data = {
-  onclick: {
-    fetch: function(e) {
+  onSaveSong: e =>
+    m.request({
+      method: "POST",
+      url: "/api/song/",
+      body: { id: e.target.value }
+    }),
+  songs: {
+    list: [],
+    fetch: function() {
       m.request({
-        method: "POST",
-        url: "/song/",
-        body: { id: e.target.value }
+        method: "GET",
+        url: "/api/songs"
+      }).then(function(items) {
+        Data.songs.list = items;
       });
     }
   }
 };
 
-const Todos = {
-  view: function(vnode) {
-    return m("form", [
+const SaveSong = {
+  view: vnode =>
+    m("form", [
       m("label.label", "Song Id"),
       m("input.input[type=text][placeholder=Song Id]"),
-      m("button.button[type=button]", { onclick: Data.onclick.fetch }, "Save")
-    ]);
-  }
+      m("button.button[type=button]", { onclick: Data.onSaveSong }, "Save")
+    ])
+};
+
+const Songs = {
+  oninit: Data.songs.fetch,
+  view: vnode =>
+    m(
+      "div",
+      Data.songs.error
+        ? [m(".error", Data.songs.error)]
+        : Data.songs.list
+        ? [
+            Data.songs.list.map(function(item) {
+              return m("div", item._id);
+            })
+          ]
+        : m(".loading-icon")
+    )
 };
 
 m.route(document.body, "/", {
-  "/": Todos
+  "/": SaveSong,
+  "/songs": Songs
 });
--- lunaB/index.js
+++ lunaB/index.js
@@ -4,6 +4,7 @@ const Router = require("koa-router");
 const BodyParser = require("koa-bodyparser");
 const logger = require("koa-logger");
 const mount = require("koa-mount");
+const send = require("koa-send");
 const app = new Koa();
 const router = new Router();
 
@@ -12,29 +13,17 @@ app.use(BodyParser());
 app.use(logger());
 app.use(router.routes()).use(router.allowedMethods());
 
-const frontEnd = new Koa();
-frontEnd.use(serve("../dist/."));
+app.use(serve("./dist"));
 
-app.use(mount("/", frontEnd));
-
-router.get("/users", async ctx => {
-  ctx.body = await ctx.app.users.find().toArray();
-});
-
-router.get("/songs", async ctx => {
-  ctx.body = await ctx.app.songs.find().toArray();
-});
-
-router.get("/song/:id", async ctx => {
-  const { id } = ctx.params;
-  ctx.body = id;
-});
-
-router.post("/song/", async ctx => {
+router.post("/api/song/", async ctx => {
   const { id } = ctx.request.body;
-  console.log(id);
   ctx.body = id;
   ctx.app.songs.insert({ id });
+});
+
+router.get("/api/songs", async ctx => {
+  const songs = await ctx.app.songs.find({}).toArray();
+  ctx.body = songs;
 });
 
 app.listen(5000);
