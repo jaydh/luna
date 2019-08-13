6aa419e95b34d7d093e476107ce7ca2b31c83fdf
Jay Howard <jay@crickethealth.com>
2019-08-12 20:42:55 -0700

    Revove unused logic

--- components/MainView.js
+++ components/MainView.js
@@ -3,18 +3,9 @@ import Player from "./Player";
 import Library from "./Library";
 import Queue from "./Queue";
 import Progress from "./Progress";
-import { SS } from "../app";
 
 const MainView = initialVnode => {
   const goToSearch = () => m.route.set("/search");
-
-  const oncreate = vnode => {
-    const spotifyCode = m.route.param("code");
-    console.log("sdfaasdf", spotifyCode);
-    if (spotifyCode) {
-      SS.emit("auth", spotifyCode).then(() => m.route.set("/"));
-    }
-  };
 
   return {
     view: vnode =>
@@ -23,8 +14,7 @@ const MainView = initialVnode => {
         m(Queue),
         m(Progress),
         m(Player),
-        m("button", { onclick: goToSearch }, "Search"),
-        m("button", { onclick: oncreate }, "spot")
+        m("button", { onclick: goToSearch }, "Search")
       ])
   };
 };
